ddf70c3b89af42167b7fb8e534767b4f0c7e9422
Affan Hasan <affan.hasan88@gmail.com>
2014-11-27 10:32:58 +0500

    First commit

---
+++ src/rc_task_management/Rc_task_management.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rc_task_management;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author root
+ */
+public class Rc_task_management extends Application {
+    
+    private VBox root;
+
+    private void init(Stage primaryStage){
+        root = new VBox();
+        
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Royal Cyber Task Management");
+        primaryStage.setScene(new Scene(root, 1500, 700));
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.init(primaryStage);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
